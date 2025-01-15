5be81e6f0853a651cef1d702e83e2fa59d382202
bipinyadav12 <159446360+bipinyadav12@users.noreply.github.com>
2025-01-15 17:01:46 +0530

    Create ItemList.js

---
+++ src/components/ItemList.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function ItemList({ items }) {
+  return (
+    <div>
+      <h1>Item List</h1>
+      <ul>
+        {items.map((item) => (
+          <li key={item.id}>
+            <Link to={`/items/${item.id}`}>{item.name}</Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default ItemList;
